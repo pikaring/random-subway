c60850f180ab85309d190c99686d375a51594876
pikaring <pikaring@gmail.com>
2024-03-08 15:26:11 +0900

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,4 +1,9 @@
 // timer.js
+
+function zeroPadding(NUM, LEN){
+	return ( Array(LEN).join('0') + NUM ).slice( -LEN );
+}
+
 document.getElementById('start').addEventListener('click', function () {
     const hours = parseInt(document.getElementById('hours').value, 10);
     const minutes = parseInt(document.getElementById('minutes').value, 10);
@@ -19,7 +24,8 @@ document.getElementById('start').addEventListener('click', function () {
         const percentComplete = ((totalSeconds - remainingSeconds) / totalSeconds) * 100;
         progressBar.style.width = percentComplete + '%';
 
-        const displayHours = Math.floor(remainingSeconds / 3600);
+        const displayHours = zeroPadding(Math.floor(remainingSeconds / 3600),2);
+//        const displayHours = Math.floor(remainingSeconds / 3600);
         const displayMinutes = Math.floor((remainingSeconds % 3600) / 60);
         const displaySeconds = remainingSeconds % 60;
         countdownDisplay.textContent = `${displayHours}:${displayMinutes}:${displaySeconds}`;
